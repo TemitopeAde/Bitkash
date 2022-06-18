9569d670998e14eca406f80330ea750a9b9ffc77
Temitope <adesiyantope2014@gmail.com>
2022-06-18 14:54:03 +0100

    works

---
+++ src/components/HowItWorks.js
@@ -0,0 +1,43 @@
+import { Box, Container } from "@mui/material";
+import React from "react";
+import One from "../assets/images/one.png";
+import Two from "../assets/images/two.png";
+import Three from "../assets/images/three.png";
+
+import "./howitworks.css";
+
+const HowItWorks = () => {
+  return (
+    <Box className="section-2">
+      <Container>
+        <h3 className="text-center title">How it Works</h3>
+
+        <div className="mt-5 flex-box">
+          <div className="flexitems">
+            <div className="number">
+              <img src={One} alt="one" />
+            </div>
+            <h3>Sign up</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex.</p>
+          </div>
+          <div className="flexitems">
+            <div className="number">
+              <img src={Two} alt="one" />
+            </div>
+            <h3>KYC</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex.</p>
+          </div>
+          <div className="flexitems">
+            <div className="number">
+              <img src={Three} alt="one" />
+            </div>
+            <h3>Buy</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex.</p>
+          </div>
+        </div>
+      </Container>
+    </Box>
+  );
+};
+
+export default HowItWorks;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,14 @@ import { Box } from '@mui/material';
 import React from 'react';
 import Hero from '../components/Hero';
 import SectionOne from '../components/SectionOne';
+import HowItWorks from '../components/HowItWorks';
 
 const Home = () => {
   return (
     <Box>
       <Hero />
       <SectionOne />
+      <HowItWorks />
     </Box>
   )
 }
